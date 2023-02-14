052645ceb6468d72414e72987f804ab25b9b3b97
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-02-14 15:31:04 +0800

    feat: make Step1.jsx render on page:
    建立 Step1.jsx 到 src/index.jsx 之間的輸出、引入，
    先確認能將 Step1.jsx 呈現於畫面上

---
+++ src/components/Main/Step/Part1.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Part1() {
+  return <h1>123</h1>;
+}
+
+export default Part1;
--- src/components/Main/Step/index.jsx
+++ src/components/Main/Step/index.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Progress from "./Progress";
+import Part1 from "./Part1";
 
 function Step() {
-  return <Progress />;
+  return (
+    <>
+      <Progress />
+      <Part1 />
+    </>
+  );
 }
 
 export default Step;
