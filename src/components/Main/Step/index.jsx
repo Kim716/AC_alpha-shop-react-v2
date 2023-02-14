2a9bd2af0b0a884241465b36f8a2f75d58a78235
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-02-15 06:26:45 +0800

    feat: make Controller.jsx render on page:
    建立 Controller.jsx 到 src/index.jsx 之間的輸出、引入，
    先確認能將 Controller.jsx 呈現於畫面上

--- src/components/Main/Step/index.jsx
+++ src/components/Main/Step/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Progress from "./Progress";
 import Part1 from "./Part1";
+import Controller from "./Controller";
 
 function Step() {
   return (
     <>
       <Progress />
       <Part1 />
+      <Controller />
     </>
   );
 }
