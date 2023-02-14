3fe41fc113ded705a03ffd6065874f3c885a79c1
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-02-14 15:21:08 +0800

    feat: extract Progress component:
    將左側步驟標題與進度條提取成一個元件

--- src/components/Main/Step/Progress.jsx
+++ src/components/Main/Step/Progress.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import iconCheck from "../../../assets/icons/ic_baseline-check.svg";
+
+function ProgressGroup({ phase, order, label }) {
+  return (
+    <>
+      <span className="progress-group" data-phase={phase}>
+        <span className="progress-icon">
+          <span className="text">{order}</span>
+          <img src={iconCheck} alt="iconCheck" />
+          {/* !!! 要補 className
+          <svg className="icon cursor-point">
+            <use xlink:href="#svg-icon-pg-complete"></use>
+          </svg> 
+          */}
+        </span>
+        <span className="progress-label">{label}</span>
+      </span>
+      {order !== 3 && <span className="progress-bar" data-order={order}></span>}
+    </>
+  );
+}
 
 function Progress() {
-  return <h1>123</h1>;
+  return (
+    <>
+      {/* <!-- register-title --> */}
+      <h2 className="register-title col col-12">結帳</h2>
+
+      {/* <!-- register-progress --> */}
+      <section className="progress-container col col-12">
+        <ProgressGroup phase="address" order={1} label="寄送地址" />
+
+        <ProgressGroup phase="shipping" order={2} label="運送方式" />
+
+        <ProgressGroup phase="shipping" order={3} label="付款資訊" />
+      </section>
+    </>
+  );
 }
 
 export default Progress;
